0df160c3abe4c275b68e376f4870a5522b2b962d
Emon <Emon@DESKTOP-98TRJS2>
2024-03-27 15:51:00 +0600

    commit

--- GraphQL.WebApi/GraphQL/Queries/AgentListQuery.cs
+++ GraphQL.WebApi/GraphQL/Queries/AgentListQuery.cs
@@ -14,6 +14,32 @@ namespace GraphQL.WebApi.GraphQL.Queries
         private readonly ApplicationDbContext _appContext;
         public AgentListQuery(ApplicationDbContext appContext)
         {
+            // this._appContext = appContext;
+            // Name = "Query";
+            // Field<ListGraphType<AgentListQueryGraphType>>("agentlists", "Returns a list of AgentList", resolve: context => _appContext.AgentList.ToList());
+            // Field<AgentListQueryGraphType>("agentlist", "Returns a Single Agent List",
+            //     new QueryArguments(new QueryArgument<NonNullGraphType<IntGraphType>> { Name = "id", Description = "Id" }),
+            //         context => _appContext.AgentList.Single(x => x.Id == context.Arguments["id"].GetPropertyValue<int>()));
+
+            // Field<ListGraphType<AgentListQueryGraphType>>("getAgentListWithBalance", "Returns Agent List",
+            // new QueryArguments(new QueryArgument<StringGraphType> { Name = "wallet", Description = "wallet" }),
+            // context => _appContext.AgentList.Where(x => x.RAWallet == context.Arguments["wallet"].GetPropertyValue<string>()));
+
+
+            // Field<ListGraphType<IdentityQueryGraphType>>("getIdentity", "Returns Identity",
+            // new QueryArguments(new QueryArgument<StringGraphType> { Name = "wallet", Description = "wallet" }),
+            // context => _appContext.AgentList.Where(x => x.RAWallet == context.Arguments["wallet"].GetPropertyValue<string>()));
+
+
+            ////Field<ListGraphType<CurrentBalanceQueryGraphType>>("getCurrentBalance", "Returns Current Balance",
+            //// new QueryArguments(new QueryArgument<StringGraphType> { Name = "identityId", Description = "identityId" }),
+            //// context => _appContext.AgentList.Where(x => x.identityId == context.Arguments["identityId"].GetPropertyValue<string>()));
+
+
+            //Field<CurrentBalanceQueryGraphType>("getCurrentBalance", "Returns Current Balance",
+            // new QueryArguments(new QueryArgument<StringGraphType> { Name = "identityId", Description = "identityId" }),
+            // context => _appContext.AgentList.Single(x => x.identityId == context.Arguments["identityId"].GetPropertyValue<string>()));
+
             this._appContext = appContext;
             Name = "Query";
             Field<ListGraphType<AgentListQueryGraphType>>("agentlists", "Returns a list of AgentList", resolve: context => _appContext.AgentList.ToList());
@@ -26,14 +52,15 @@ namespace GraphQL.WebApi.GraphQL.Queries
             context => _appContext.AgentList.Where(x => x.RAWallet == context.Arguments["wallet"].GetPropertyValue<string>()));
 
 
-            Field<ListGraphType<IdentityQueryGraphType>>("getIdentity", "Returns Identity",
+            Field<IdentityQueryGraphType>("getIdentity", "Returns Identity",
             new QueryArguments(new QueryArgument<StringGraphType> { Name = "wallet", Description = "wallet" }),
-            context => _appContext.AgentList.Where(x => x.RAWallet == context.Arguments["wallet"].GetPropertyValue<string>()));
+            context => _appContext.AgentList.FirstOrDefault(x => x.RAWallet == context.Arguments["wallet"].GetPropertyValue<string>()));
 
 
-           Field<ListGraphType<CurrentBalanceQueryGraphType>>("getCurrentBalance", "Returns Current Balance",
-            new QueryArguments(new QueryArgument<StringGraphType> { Name = "identityId", Description = "identityId" }),
-            context => _appContext.AgentList.Where(x => x.identityId == context.Arguments["identityId"].GetPropertyValue<string>()));
+            Field<CurrentBalanceQueryGraphType>("getCurrentBalance", "Returns Current Balance",
+             new QueryArguments(new QueryArgument<StringGraphType> { Name = "identityId", Description = "identityId" }),
+             context => _appContext.AgentList.FirstOrDefault(x => x.identityId == context.Arguments["identityId"].GetPropertyValue<string>()));
+
 
         }
     }
